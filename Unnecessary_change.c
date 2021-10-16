89a96e6c8c2c8ab4a030d1a3c91b01578bcf670c
CH4B3L0 <89417450+CH4B3L0@users.noreply.github.com>
2021-10-16 09:11:57 -0500

    just because

---
+++ Unnecessary_change.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+# define size 10
+int array[size] = {3,6,9,12,15,18,21,24,27,30}; 
+
+int insertz(int a, int stop)
+{
+    int cociente, resto;
+    if ( a > 0 && stop > 0 )
+    {
+        cociente = 0;
+        resto = a;
+
+        while ( resto >= stop )
+        {
+            resto = resto - stop;
+            cociente = cociente +1;
+        }
+    }
+    return cociente;
+}
+
+int change(int *a,int b)
+{
+    int stop = b/2;
+    int temp = 0;
+    int result = 0;
+    for(int i=0; i< b ;i++)
+    {
+        temp = insertz(a[i], stop);
+        result = result + temp;
+    }
+    return result;
+};
+
+int main()
+{
+    int A = change(array,size);
+    printf("%d", A);
+    return A;
+};
